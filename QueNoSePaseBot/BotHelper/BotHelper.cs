a17311d3af005e227c29757a91b16b9363edeb63
Facu Villar <facuandroid.fv@gmail.com>
2016-08-28 13:53:42 -0300

    First v3 bot

---
+++ QueNoSePaseBot/BotHelper/BotHelper.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.Bot.Connector;
+
+namespace QueNoSePaseBot.BotHelper
+{
+    internal class BotHelper
+    {
+        public static string ParseMessage(Activity message)
+        {
+            try
+            {
+                var msg = message.Text;
+                var msgLower = msg.ToLower();
+                if (msg.Trim().ToLower().Equals("hola"))
+                    return "Buen día, " + message.From.Name;
+
+                if (msg.Trim().ToLower().Equals("ayuda") || msg.Trim().ToLower().Equals("opciones") || msg.Trim().ToLower().Equals("opcion"))
+                {
+                    StringBuilder sb = new StringBuilder();
+                    //sb.AppendLine(
+                        //"Envía: [Número de Parada] [espacio] [Número de Línea] para conocer el arribo por línea.");
+                    //sb.AppendLine("");
+                    sb.AppendLine(
+                        "Envía: [Número de Parada] [espacio] todos para conocer el arribo de todas las líneas en esa parada.");
+                    sb.AppendLine("");
+                    sb.AppendLine("Ejemplo:");
+                    sb.AppendLine("  C1324");
+                    return sb.ToString();
+                }
+
+                // buscar parada
+                if (msgLower.StartsWith("c"))
+                {
+                    var splitted = msgLower.Split(' ');
+                    //si comienza con c y nada mas, asume busqueda por todos
+                    if (splitted.Length == 1)
+                    {
+                        var list = HttpHelper.GetHorarioTodos(splitted[0]);
+                        if (list != null)
+                        {
+                            return list;
+                        }
+
+                        return "Error";
+                    }
+                    else if (splitted.Length > 1)
+                    {
+                        var parada = splitted[0];
+                        var linea = splitted[1];
+
+                        if (linea.ToLower().Equals("todos") || linea.ToLower().Equals("todas"))
+                        {
+                            var list = HttpHelper.GetHorarioTodos(parada);
+                            if (list != null)
+                            {
+                                return list;
+                            }
+
+                            return "Error";
+                        }
+
+                        //return "Parada: " + parada + ", Linea: " + linea;
+                    }
+                    return "Disculpe, faltan datos para poder realizar su consulta.";
+                }
+                return "Disculpe, no hemos podido procesar su consulta. Verifique que los datos sean correctos";
+            }
+            catch (Exception ex)
+            {
+                return "Disculpe, no hemos podido procesar su consulta. Verifique que los datos sean correctos";
+            }
+        }
+    }
+
+    internal class HttpHelper
+    {
+        private static string USER_ID = "7fb167c1-0b3f-4abb-9408-519e2d3576d1";
+        private static string USUARIO = "usuarioefisat";
+        private static string CLAVE = "efisat";
+        private static int PAIS = 1;
+        private static int PROVINCIA = 17;
+        private static int CIUDAD = 20;
+
+        public static string GetHorarioTodos(string parada)
+        {
+            try
+            {
+                var a = new ServicioWebHorariosProximos.ServicioWebHorariosProximos();
+                var res = a.RecuperarHorarioCuandoLlegaDosAndroid(parada, USER_ID, "TODOS", USUARIO, CLAVE, "-1");
+                var lista = new List<Horario>();
+                if (res.StartsWith("0|"))
+                {
+                    var resAux = res.Split('|');
+                    if (resAux.Length < 2) return null;
+
+                    var sp = resAux[1].Split(new[] { "//" }, StringSplitOptions.None);
+
+                    foreach (string s in sp)
+                    {
+                        if (string.IsNullOrEmpty(s)) continue;
+
+                        var data = s.Split(',');
+                        if (data.Length > 0)
+                        {
+                            Horario horario = new Horario();
+                            horario.Linea = data[0];
+                            horario.Barrio = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(data[1].ToLower());
+                            if (data[2] == "0")
+                            {
+                                horario.Llegando = true;
+                                horario.Minutos = 0;
+                            }
+                            else
+                                horario.Minutos = Int32.Parse(data[2]);
+
+                            lista.Add(horario);
+                        }
+                    }
+                }
+
+                var listaOrdered = lista.OrderBy(item => item.Minutos);
+
+                //ServicioWebHorariosProximos a = new ServicioWebHorariosProximos();
+                //var res = a.RecuperarHorarioCuandoLlegaAndroid(parada, "7fb167c1-0b3f-4abb-9408-519e2d3576d1", "TODOS",
+                //    "usuarioefisat", "efisat", "-1");
+                //var lista = Parser.Parse(res);
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("Parada: " + parada.ToUpper() + " - Todos - ");
+                sb.AppendLine(Environment.NewLine);
+                foreach (Horario horario in listaOrdered)
+                {
+                    sb.AppendLine(horario.ToString());
+                    sb.AppendLine(Environment.NewLine);
+                }
+                return sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+
+    internal class Parser
+    {
+        public static List<Horario> Parse(string text)
+        {
+            var resAux = text.Split('|');
+            if (resAux.Length < 2) return null;
+
+            var sp = resAux[1].Split(new[] { "//" }, StringSplitOptions.None);
+
+            var banned = new List<string>();
+            banned.Add("A");
+            banned.Add("ARRIBANDO");
+
+            var horarios = new List<Horario>();
+
+            Regex regex = new Regex(@"\b([A-Z]+)\b");
+            foreach (string s in sp)
+            {
+                if (string.IsNullOrEmpty(s)) continue;
+
+                var horario = new Horario();
+
+                var linea = s.Substring(0, 3).Trim();
+                var hacia = "";
+                var matches = regex.Matches(s);
+                foreach (Match match in matches)
+                {
+                    if (!banned.Contains(match.Value))
+                    {
+                        hacia += match.Value + " ";
+                    }
+                    else if (match.Value == "ARRIBANDO")
+                    {
+                        horario.Llegando = true;
+                    }
+                }
+
+                horario.Linea = linea;
+                horario.Barrio = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(hacia.ToLower());
+                horario.Barrio = horario.Barrio.TrimEnd();
+
+                var regexMin = new Regex(@"( [0-9]+ [a-z]+)");
+                var matchesMin = regexMin.Matches(s);
+                foreach (Match match in matchesMin)
+                {
+                    //horario.Minutos = match.Value;
+                }
+                horarios.Add(horario);
+            }
+
+            //var json = JsonConvert.SerializeObject(horarios);
+            return horarios;
+        }
+    }
+
+    internal class Horario
+    {
+        public string Linea { get; set; }
+        public string Barrio { get; set; }
+        private string MinutosText
+        {
+            get
+            {
+                return Minutos + " min";
+            }
+        }
+        public int Minutos { get; set; }
+        public bool Llegando { get; set; }
+
+        public override string ToString()
+        {
+            var s = new StringBuilder();
+            s.Append("Linea: **" + Linea + "**");
+            s.Append(", " + Barrio);
+            if (Llegando)
+                s.Append(", **Llegando**");
+            else
+                s.Append(", en **" + MinutosText + "**");
+
+            return s.ToString();
+        }
+    }
+}
---
+++ QueNoSePaseBot/Controllers/MessagesController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+
+namespace QueNoSePaseBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                Activity reply = activity.CreateReply("Disculpe, no hemos podido procesar su consulta. Verifique que los datos sean correctos");
+                int state = 0;
+
+                if (activity.Attachments.Count > 0)
+                {
+                    reply = activity.CreateReply("Disculpe, sólo se aceptan mensajes de texto");
+                    state = 1;
+                }
+
+                if (string.IsNullOrEmpty(activity.Text) && state == 0)
+                {
+                    //CardAction action = new CardAction
+                    //{
+                    //    Type = "openUrl",
+                    //    Title = "Ver Paradas Cercanas",
+                    //    Value = "http://map.quenosepase.com.ar/?user=-33.5650168;-64.8363918&paradas=1,Linea%2010,C1234,-31.387147;-64.217691|2,Linea%2022,C4444,-31.384493;-64.200605"
+                    //};
+                    //activity.Attachments.Add(new Attachment
+                    //{
+                        
+                    //});
+                    reply = activity.CreateReply("Ups.. Mensaje vacío!");
+                    state = 2;
+                }
+
+                if (state == 0)
+                {
+                    var newMsg = BotHelper.BotHelper.ParseMessage(activity);
+                    reply = activity.CreateReply(newMsg);
+                }
+
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+                Activity reply = message.CreateReply();
+                reply.Type = "Ping";
+                return reply;
+            }
+
+            return null;
+        }
+    }
+}
